97693993857d629fedada691607b5bec284cf82b
exit <saraswathi.mac@gmail.com>
2022-10-12 22:24:11 +0530

    Added starter files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      Indian History
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
