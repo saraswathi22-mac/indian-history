474310fbbb6ec560a09bdd0c96606465f04b6323
exit <saraswathi.mac@gmail.com>
2022-12-26 16:19:29 +0530

    Added contents for making history

--- src/common/Headings/Headings.tsx
+++ src/common/Headings/Headings.tsx
@@ -9,10 +9,18 @@ type Heading = {
   heading: string;
 };
 
+type SubHeading = {
+  subHeading: string;
+};
+
 export const MainHeading = ({ mainHeading }: MainHeading) => {
   return <div className="main_heading">{mainHeading}</div>;
 };
 
 export const Heading = ({ heading }: Heading) => {
   return <div className="heading">{heading}</div>;
+};
+
+export const SubHeading = ({ subHeading }: SubHeading) => {
+  return <div className="sub_heading">{subHeading}</div>;
 };
--- src/components/AncientHistory/AncientHistory.tsx
+++ src/components/AncientHistory/AncientHistory.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "../../common/Button/Button";
-import { MainHeading } from "../../common/Headings/Headings";
+import { MainHeading, SubHeading } from "../../common/Headings/Headings";
 import ImageSlider from "../../common/ImageSlider/ImageSlider";
 import { SliderData } from "../../common/ImageSlider/SliderData";
 
@@ -8,6 +8,11 @@ const AncientHistory = () => {
   return (
     <div className="history">
       <MainHeading mainHeading="Ancient History" />
+      <SubHeading subHeading="Making History"/>
+      <li className="subHeading">PreHistory - no history, no written doc</li>
+      <li className="subHeading">Stone age: 2million - 4000BC b. Copper age/charcolithic: 4000-1500BC c. Iron age: 1500BC-600BC</li>
+      <li className="subHeading">Protohistory - records but cannot be understood; indus valley civilization etc</li>
+      <li className="subHeading">History - with understandable records; medieval, modern history</li>
       <Button
         locate="/ancient_history/sources_ancient_history"
         title="Sources of Ancient History"
